7825e827f3ad1b1dde9e2d704611f6216b568c22
unknown <anderson04garciamail.com>
2024-09-08 17:21:01 -0500

    Agregación del proyecto Backend de la actividad

---
+++ movie-app-backend/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const db = require('./config/db');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Rutas
+app.use('/genero', require('./routes/genero'));
+app.use('/director', require('./routes/director'));
+app.use('/productora', require('./routes/productora'));
+app.use('/tipo', require('./routes/tipo'));
+app.use('/media', require('./routes/media'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
