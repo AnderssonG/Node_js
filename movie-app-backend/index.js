0399f4deb40f34a1c31de30ba2b2371ba98c5c3d
AnderssonG <anderson04garcia@gmail.com>
2024-10-06 16:34:48 -0500

    Correccion de puerto 5

--- movie-app-backend/index.js
+++ movie-app-backend/index.js
@@ -16,8 +16,10 @@ app.use('/productora', require('./routes/productora'));
 app.use('/tipo', require('./routes/tipo'));
 app.use('/media', require('./routes/media'));
 
-
-;
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
 
 
 
