1ad685a205b222593572f57d24024adb8aeee1d2
AnderssonG <anderson04garcia@gmail.com>
2024-10-06 16:07:15 -0500

    Correccion de puerto

--- movie-app-backend/index.js
+++ movie-app-backend/index.js
@@ -16,10 +16,7 @@ app.use('/productora', require('./routes/productora'));
 app.use('/tipo', require('./routes/tipo'));
 app.use('/media', require('./routes/media'));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+;
 
 
 
